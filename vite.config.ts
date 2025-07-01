86afdb1d68f49bcb3203ed744e96fcb16f6021ae
Kaavya Radhakrishnan <103844077+kaavyar9705@users.noreply.github.com>
2025-07-01 18:14:20 +0000

    Rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/kaavya-portfolio2/",
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
