5e9d2dfefdbaa41f0420730e92bfaecede5ec2d4
Kaavya Radhakrishnan <103844077+kaavyar9705@users.noreply.github.com>
2025-07-08 14:42:22 +0000

    base changed

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/kaavya-portfolio2/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
